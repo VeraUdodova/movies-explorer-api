ec7913be804e0c230a338858b9a68b38e1b849f2
VeraUdodova <udodowa.vera@yandex.ru>
2023-06-03 19:41:04 +0300

    user validator

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ const NotAuthorizedError = require('../errors/not-authorized');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
     minlength: 2,
     maxlength: 30,
   },
